b2ef6c3ed34d6195ce43f406752ca5bda1eefa5c
Hillary owusu Ameyaw <124349494+HillaryOwusu23@users.noreply.github.com>
2024-05-17 02:45:28 -1200

    added jest testing library and MetaData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
